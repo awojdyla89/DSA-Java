4b2cc6084d53f27d032cfa38c5db259ab90e0645
Adam Wojdyla <awojdyla@gmail.com>
2022-12-08 15:44:04 -0800

    Alternate method to solve Contains Duplicate added

--- src/ContainsDuplicate.java
+++ src/ContainsDuplicate.java
@@ -14,18 +14,37 @@ Output: true
 */
 
 /*
+HashMap
 create a hashmap to store key:value of number: count
 if we come across a number that is seen again we increment the count
 if the count >= 2 then we know we found a number that matches the criteria.
 
+HashSet
+Add all nums array to hash set. This will eleminate any duplicates
+return true if the len nums != len of hash set. Meaning duplicates were found
+
 Time = O(n) Worst case we traverse the entire nums array and find no duplicates
- Space = O(n) Worst case adding all unique numbers to hashmap
+Space = O(n) Worst case adding all unique numbers to hashmap
  */
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class ContainsDuplicate {
 
+//   Implemented with a hash set which eliminates any duplicates
+    public boolean ContainsDupeSet(int[] nums){
+        HashSet<Integer> bank = new HashSet<>();
+        for(int num: nums){
+            bank.add(num);
+        }
+//        System.out.println("Hash set: " + bank + " Nums Array " + Arrays.toString(nums));
+//        System.out.println("Len of hashset: " + bank.size() + " Len of nums array: " + nums.length);
+        return nums.length != bank.size();
+    }
+
+//    Implemented using a hash map to store a counter
     public boolean ContainsDupe(int[] nums){
 //        System.out.println("Nums to check: " + Arrays.toString(nums));
         HashMap<Integer, Integer> bank = new HashMap<>();
@@ -52,10 +71,15 @@ public class ContainsDuplicate {
         int[] nums2 = {1,2,3,4};
         int[] nums3 = {1,1,1,3,3,4,3,2,4,2};
 
+//        Hash map
         System.out.println(sol.ContainsDupe(nums1) + "\n");
         System.out.println(sol.ContainsDupe(nums2) + "\n");
-        System.out.println(sol.ContainsDupe(nums3));
+        System.out.println(sol.ContainsDupe(nums3) + "\n");
 
+//        Hash Set
+        System.out.println(sol.ContainsDupeSet(nums1) + "\n");
+        System.out.println(sol.ContainsDupeSet(nums2) + "\n");
+        System.out.println(sol.ContainsDupeSet(nums3));
 
     }
 }
