e8946a807c423459ca40405088040c0e6d0d227b
Adam Wojdyla <awojdyla@gmail.com>
2022-12-20 20:08:53 -0700

    optimizes reverse vowels algo

--- src/ReverseVowelsString.java
+++ src/ReverseVowelsString.java
@@ -5,42 +5,58 @@ public class ReverseVowelsString {
 
     public String ReverseVowelsInString(String str){
         Map<Character, Integer> bank = new HashMap<>();
-        bank.put('a',1);
-        bank.put('e',2);
-        bank.put('i',3);
-        bank.put('o',4);
-        bank.put('u',5);
-        System.out.println(bank);
+        bank.put('a',0);
+        bank.put('e',0);
+        bank.put('i',0);
+        bank.put('o',0);
+        bank.put('u',0);
+        bank.put('A',0);
+        bank.put('E',0);
+        bank.put('I',0);
+        bank.put('O',0);
+        bank.put('U',0);
 
         int left = 0;
         int right = str.length() - 1;
-        System.out.println(right);
+        char[] carr = str.toCharArray();
 
         while (left <= right){
-//            int mid = left + (right + left) / 2;
 
-            if (bank.containsKey(str.charAt(left)) && bank.containsKey(str.charAt(right))){
-                System.out.println("left: " + str.charAt(left) + " right: " + str.charAt(right));
-                //TODO: swap the vowels then increment the pointers
+            boolean leftIsVowel = bank.containsKey(carr[left]);
+            boolean rightIsVowel = bank.containsKey(carr[right]);
+
+            if (leftIsVowel && rightIsVowel){
+//                System.out.println("left: " + carr[left] + " right: " + carr[right]);
+                char temp = carr[left];
+                carr[left] = carr[right];
+                carr[right] = temp;
+
                 left += 1;
                 right -= 1;
             }
-            if(!bank.containsKey(str.charAt(left))){
+            if(!leftIsVowel){
                 left += 1;
             }
-            if(!bank.containsKey(str.charAt(right))){
+            if(!rightIsVowel){
                 right -= 1;
-
             }
-
         }
-        return "null";
+        return new String(carr);
     }
+
 
     public static void main(String[] args) {
         ReverseVowelsString rev = new ReverseVowelsString();
 
-        String str = "hello";
-        rev.ReverseVowelsInString(str);
+        String str1 = "hello";
+        String str2 = "leetcode";
+        String str3 = "aeiou";
+        String str4 = "AbBa";
+
+        System.out.println(rev.ReverseVowelsInString(str1));
+        System.out.println(rev.ReverseVowelsInString(str2));
+        System.out.println(rev.ReverseVowelsInString(str3));
+        System.out.println(rev.ReverseVowelsInString(str4));
+
     }
 }
